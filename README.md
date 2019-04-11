a13039d3b057de215d0d6dcb3779b1f8dcd4315c
Arthur A. Bergamaschi <arthurbergmz@gmail.com>
2019-04-11 03:56:30 -0300

    :tada: First commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# arthurbergamaschi
+
+> Personal website
+
+Built upon the experimental design proposed by [yiara.design](https://yiara.design/).
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:1234
+npm run serve
+
+# build for production
+npm run build
+```
---
+++ app/index.js
@@ -0,0 +1,72 @@
+import './style/_build.scss'
+
+function getElementsByClass(elementClass) {
+  return elementClass.reduce((obj, clazz) => {
+    const el = document.getElementsByClassName(clazz)[0]
+    obj[clazz] = {
+      click: (handler) => el.addEventListener('click', (event) => handler(event, el)),
+      css: (property, value) => value === undefined ? el.style[property] : el.style[property] = value === null ? 0 : value,
+      el
+    }
+    return obj
+  }, {})
+}
+
+function reactivity (initialState) {
+  const stateMachine = Object.getOwnPropertyNames(initialState).reduce((machine, property) => {
+    const propertyObj = initialState[property]
+    machine._state[property] = propertyObj.value
+    machine._core[property] = { _lastValue: propertyObj.value, _handler: propertyObj.handler }
+    return machine
+  }, { _state: {}, _core: {} })
+  return new Proxy(stateMachine._state, {
+    set (target, property, value, receiver) {
+      const propertyCore = stateMachine._core[property]
+      propertyCore._handler((propertyCore._lastValue = target[property]), value)
+      return Reflect.set(target, property, value, receiver)
+    }
+  })
+}
+
+const appElements = getElementsByClass([
+  'app-wrapper', 'app-menu', 'app-menu__header', 'app-menu__back-button', 'app-menu__show-button', 'app-menu__body', 'app-menu__body-wrapper'
+])
+
+const appProperties = Object.seal({
+  mobileMenuMaxHeight: appElements['app-menu__body'].el.getBoundingClientRect().height
+})
+
+const appState = reactivity({
+  menuOpen: {
+    value: false,
+    handler (_, value) {
+      if (value) {
+        appElements['app-wrapper'].el.classList.add('app-wrapper--unfocused')
+        appElements['app-menu'].el.classList.add('app-menu--active')
+        appElements['app-menu__show-button'].el.classList.add('app-menu__show-button--show-less')
+        appElements['app-menu__body-wrapper'].css('max-height', appProperties.mobileMenuMaxHeight + 'px')
+      } else {
+        appElements['app-wrapper'].el.classList.remove('app-wrapper--unfocused')
+        appElements['app-menu'].el.classList.remove('app-menu--active')
+        appElements['app-menu__show-button'].el.classList.remove('app-menu__show-button--show-less')
+        appElements['app-menu__body-wrapper'].css('max-height', 0)
+      }
+    }
+  }
+})
+
+appElements['app-menu__header'].click((e, self) => {
+  if (e.target !== self) {
+    return e.preventDefault()
+  }
+  appState.menuOpen = !appState.menuOpen
+})
+
+appElements['app-menu'].click((e, self) => {
+  if (e.target !== self) {
+    return e.preventDefault()
+  }
+  appState.menuOpen = false
+})
+
+console.log('Hello, world!')
